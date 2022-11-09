ea8666840b4a9ea7a1acf461175ebb5218ba51c8
Amaechi-kizito <tochukwuamaechi16@gmail.com>
2022-11-09 10:47:47 +0100

    What changed? Customized Error 404 page.

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,18 +1,24 @@
 import React from "react"
-import {Link} from "react-router-dom"
-
+import {Link, useNavigate} from "react-router-dom"
+import oops from "../images/oops.png"
 
 function Error(){
+    const navigate = useNavigate()
+    
     return(
         <>
             
-            <div>
-                <h1>Error 404</h1>
-                <p>Page not found</p>
-            </div>
-
-            <div>
-                <Link to='/' className="btn btn-success btn-sm">Back to Home</Link>
+            <div className="row error404">
+                <div className="col-12"></div>
+                <h1 className="text-center" style={{fontSize:"80px"}}>OOPS!</h1>
+                <p className="text-center"><img src={oops} width='200px'  style={{borderRadius:"50%"}}/></p>
+                <h1 className="text-center">Error 404</h1>
+                <p className="text-center">Page not found</p><br/>
+                <p className="text-center">
+                    <button className="btn btn-warning btn-md-sm" onClick={()=>navigate(-1)}>
+                        Previous page
+                    </button>
+                    </p>
             </div>
         </>
     )
