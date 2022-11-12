b8f0c8a8586ac0548292b38077be6bb87c8720e3
Amaechi-kizito <tochukwuamaechi16@gmail.com>
2022-11-12 17:12:23 +0100

    What changed? Bug fixes. Rendering the inbox messages with dates

--- backend_api/application/routes/inbox_routes.py
+++ backend_api/application/routes/inbox_routes.py
@@ -18,23 +18,64 @@ def send_message():
     
 
 
+# @app.route("/api/v1/inbox/")
+# def inbox():
+#     developer_id=request.args.get("devId")
+#     messages=db.session.query(Inbox).filter(Inbox.msg_recipient==developer_id).all()
+    
+    
+#     inbox_info=[]
+    
+#     if messages!= []:
+#         for msg in messages:
+#             message_sender=db.session.query(User).filter(User.user_id==msg.msg_sender).first()
+#             sender_name=message_sender.user_nickname
+            
+#             msg_details={}
+#             msg_details["sender"]=sender_name
+#             msg_details["message_body"]=msg.message
+#             inbox_info.append(msg_details)
+#         return {"status":True, "message":inbox_info}
+#     else:
+#         return {"status":True, "message":"You have no mesages at this time"}
+
+
 @app.route("/api/v1/inbox/")
 def inbox():
     developer_id=request.args.get("devId")
-    messages=db.session.query(Inbox).filter(Inbox.msg_recipient==developer_id).all()
+    messages=db.session.query(Inbox).filter(Inbox.msg_recipient==developer_id).order_by(Inbox.datesent.desc()).all()
     
-    
+    allsenders=[]
+    names_of_senders=[]
+    last_message_sent=[]
     inbox_info=[]
+    timesent=[]
     
     if messages!= []:
+        #getting all distinct senders
+        for i in messages:
+            if i.msg_sender in allsenders:
+                pass
+            else:
+                allsenders.append(i.msg_sender)
+        
+        for x in allsenders:
+            the_msg_sender=db.session.query(User).filter(User.user_id==x).first()
+            name=the_msg_sender.user_nickname
+            names_of_senders.append(name)
+            
+            #getting last message sent
+            last_sent_message = db.session.query(Inbox).filter(Inbox.msg_sender==x).order_by(Inbox.datesent.desc()).first()
+            timesent.append(last_sent_message.datesent)
+            last_message_sent.append(last_sent_message.message)
+            
         for msg in messages:
             message_sender=db.session.query(User).filter(User.user_id==msg.msg_sender).first()
             sender_name=message_sender.user_nickname
-            
             msg_details={}
             msg_details["sender"]=sender_name
             msg_details["message_body"]=msg.message
             inbox_info.append(msg_details)
-        return {"status":True, "message":inbox_info}
+        return {"status":True, "message":last_message_sent, 'list_of_senders':allsenders, "names":names_of_senders, 'timestamp':timesent}
     else:
         return {"status":True, "message":"You have no mesages at this time"}
--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -2,13 +2,17 @@ import React, {useEffect, useState} from "react"
 import { Link } from "react-router-dom"
 
 const Inbox=({userSession})=>{
+    const [senders, setSenders]=useState([])
     const [myInbox, setMyInbox]=useState([])
+    const [timeDelivered, setTimeDelivered]=useState([])
     useEffect( ()=>{
         fetch(`http://localhost:5000/api/v1/inbox/?devId=${userSession}`)
         .then(rsp=>rsp.json())
         .then(data=>{
+            setSenders(data.names);
             setMyInbox(data.message);
-            //console.log(data);
+            setTimeDelivered(data.timestamp)
+            console.log(data);
         })
     },[])
 
@@ -20,7 +24,7 @@ const Inbox=({userSession})=>{
                         <div className="col-12">
                             <h2 className="mb-3" style={{color:"gold"}}>Inbox</h2><hr/>
 
-                            {(typeof myInbox === "string")?(
+                            {(typeof senders === "string")?(
                                 <div className="row" style={{minHeight:"300px", alignItems:"center"}}>
                                 <div className="col-12 text-center" style={{color:"grey"}}>
                                     <h3>Inbox Empty</h3>
@@ -31,17 +35,24 @@ const Inbox=({userSession})=>{
                             </div>
                             ):(
                                 <div>
-                                    {Object.values(myInbox).map(message=>
-                                        <div className="row">
-                                            <div className="col-12 py-3" style={{color:"white"}} key={message.message_sender}>
-                                                <div className="float-start me-2">
-                                                    <i className="fa-solid fa-user text-warning float-start fa-2x me-2" ></i>
+                                    {
+                                        senders.map( (value,key)=>(
+                                            <>
+                                                <div className="row mb-1" style={{color:"white"}} key={key}>
+                                                    <div classname="col-12">
+                                                        <div className="float-start mx-2">
+                                                            <i className="fa-solid fa-user text-warning float-start fa-3x me-2"></i>
+                                                        </div>
+                                                        <div>
+                                                            <span key={key}>{value[0].toUpperCase()  + value.substring(1)}</span>
+                                                            <span style={{fontSize:"9px"}} className="float-end">{timeDelivered[key].toLocaleString("en-IN").split(' ')[4]}</span>
+                                                            <p style={{fontSize:"12px"}}>{myInbox[key]}</p>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                {message.sender}
-                                            </div>
-                                        </div>
-                                        
-                                    )}
+                                            </>
+                                        ))
+                                    }
                                 </div>
                             )}
                         </div>
