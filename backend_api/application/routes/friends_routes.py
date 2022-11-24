74d011f09195863c9daab866275fbc8aeae27016
Amaechi-kizito <tochukwuamaechi16@gmail.com>
2022-11-24 16:58:29 +0100

    what changed? Friend requests can now be sent and accepted.

--- backend_api/application/routes/friends_routes.py
+++ backend_api/application/routes/friends_routes.py
@@ -4,31 +4,43 @@ from application.models import *
 
 @app.route('/api/v1/get-unfriended-developers/')
 def get_unfriended_devs():
-    current_dev=3
-    all_requests_users_id=[current_dev]
+    current_dev=request.args.get('currentDev')
+    all_accepted_requests_users_id=[]
+    all_pending_requests_users_id=[]
     
     # Getting the accepted requests sent out by logged in dev
     checking_req_sent_by_current_user=Friend_Request.query.filter(Friend_Request.request_sent_by==current_dev, Friend_Request.request_status=='A').all()
     if checking_req_sent_by_current_user != []:
         for sent_out in checking_req_sent_by_current_user:
-            all_requests_users_id.append(sent_out.request_sent_to)
+            all_accepted_requests_users_id.append(sent_out.request_sent_to)
             
     # Getting the accepted requests sent in i accepted
     checking_req_received_by_current_user=Friend_Request.query.filter(Friend_Request.request_sent_to==current_dev, Friend_Request.request_status=='A').all()
     if checking_req_received_by_current_user != []:
         for sent_in in checking_req_received_by_current_user:
-            all_requests_users_id.append(sent_in.request_sent_by)
+            all_accepted_requests_users_id.append(sent_in.request_sent_by)
             
+    # Getting the pending requests yet to be accepted by me
+    pending_from_me=Friend_Request.query.filter(Friend_Request.request_sent_to==current_dev, Friend_Request.request_status=='P').all()
+    if pending_from_me != []:
+        for pending in pending_from_me:
+            all_pending_requests_users_id.append(pending.request_sent_by)
+            
+    # Getting the pending requests yet to be accepted by them
+    pending_from_them=Friend_Request.query.filter(Friend_Request.request_sent_by==current_dev, Friend_Request.request_status=='P').all()
+    if pending_from_them != []:
+        for pending in pending_from_them:
+            all_pending_requests_users_id.append(pending.request_sent_to)
+    
+    combined_list= all_pending_requests_users_id + all_accepted_requests_users_id
     
     developers_list=[]
     counter=0
-    developers=db.session.query(User).all()
+    developers=db.session.query(User).filter(User.user_id != current_dev).all()
     
-    if developers != []:
+    if combined_list != []:
         for dev in developers:
-            if dev.user_id in all_requests_users_id:
-                pass
-            else:
+            if dev.user_id not in combined_list:
                 counter+=1
                 stack_id=dev.user_stack
                 stack=db.session.query(Techstack).filter(Techstack.stack_id==stack_id).first()
@@ -42,13 +54,26 @@ def get_unfriended_devs():
                 developers_list.append(dev_info)
         return {"status":True, "developers":developers_list}
     else:
-        return {"status":True, "developers":"No Developers registered at this time"}
+        for dev in developers:
+            counter+=1
+            stack_id=dev.user_stack
+            stack=db.session.query(Techstack).filter(Techstack.stack_id==stack_id).first()
+            stack_name=stack.stack_name
+            
+            dev_info={}
+            dev_info["serial_no"]=counter
+            dev_info["dev_id"]=dev.user_id
+            dev_info["dev_nickname"]=dev.user_nickname
+            dev_info["dev_stack"]=stack_name
+            developers_list.append(dev_info)
+        return {"status":True, "developers":developers_list}
     
-    
+
+
+
 #Send Friend Request   
 @app.route('/api/v1/send-friend-request/')
 def send_friend_request():
-    
     receiver_of_request=request.args.get("friendRequestRecipient")
     sender=request.args.get("userSession")
     send_friend_request=Friend_Request(request_sent_by=sender, request_sent_to=receiver_of_request, request_date=date.today())
@@ -56,4 +81,48 @@ def send_friend_request():
     db.session.commit()
     return{"status":True, "message":"Friend Request Sent"}
 
-get_unfriended_devs()
+
+
+# retrieve Friend Requests
+@app.route('/api/v1/retrieve-friend-requests/')
+def retrieve_friend_requests():
+    current_dev=request.args.get('currentDev')
+    developers_list=[]
+    counter=0
+    
+    get_all_my_pending_requests=Friend_Request.query.filter(Friend_Request.request_sent_to==current_dev, Friend_Request.request_status=='P').all()
+    
+    if get_all_my_pending_requests != []:
+        for dev in get_all_my_pending_requests:
+            dev_info=db.session.query(User).filter(User.user_id == dev.request_sent_by).first()
+            counter+=1
+            stack_id=dev_info.user_stack
+            stack=db.session.query(Techstack).filter(Techstack.stack_id==stack_id).first()
+            stack_name=stack.stack_name
+            
+            developer_info={}
+            developer_info["serial_no"]=counter
+            developer_info["dev_id"]=dev_info.user_id
+            developer_info["dev_nickname"]=dev_info.user_nickname
+            developer_info["dev_stack"]=stack_name
+            developers_list.append(developer_info)
+        return {"status":True, "developers":developers_list}
+    else:
+        return{"status":False, "developers":"No Pending Requests"}
+
+
+# Accept Friend Requests
+@app.route('/api/v1/accept-friend-request/')
+def accept_friend_requests():
+    invitee=request.args.get('invitee')
+    invited=request.args.get('invited')
+    
+    invited_deets=User.query.get(invited)
+    name_of_friend=invited_deets.user_nickname
+    captitalized_name=name_of_friend[0].upper() + name_of_friend[1:]
+    
+    #accept request process
+    accept_request=db.session.query(Friend_Request).filter(Friend_Request.request_sent_by==invited, Friend_Request.request_sent_to==invitee).first()
+    accept_request.request_status='A'
+    db.session.commit()
+    return {"status":True, "message":f"{captitalized_name} is now your friend."}
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,6 +7,7 @@ function Dashboard({userSession}){
   const [averageBugs, setAverageBugs]=useState(0)
   const [availableDevelopers, setAvailableDevelopers]=useState([])
   const [friends, setFriends]=useState("")
+  const [friendRequest, setfriendrequest]=useState([])
 
   
   //Get developer statistics
@@ -21,26 +22,31 @@ function Dashboard({userSession}){
       setFriends(statistics.friends);
     })
     .catch((error)=> console.log(error))
-
   }, [])
 
-  //Getting all developers
-  useEffect( ()=>{ 
+  //Getting unfriended developers
+  const getDevelopers=()=>{
     fetch(`http://localhost:5000/api/v1/get-unfriended-developers/?currentDev=${userSession}`)
     .then(rsp=>rsp.json())
     .then(data=>{
       setAvailableDevelopers(data.developers);
     })
-  },[])
+  }
+
+  const getFriendRequest=()=>{
+    fetch(`http://localhost:5000/api/v1/retrieve-friend-requests/?currentDev=${userSession}`)
+    .then(rsp=>rsp.json())
+    .then(data=>{
+      setfriendrequest(data.developers);
+    })
+  }
 
 
   return(
     <>
       <div className="row dashboardlinks">
-
         <div className="col-md-11">
           <h2 style={{color:"gold"}}>Statistics</h2><hr/>
-
           <div className="row" style={{minHeight:"400px", alignItems:'center', color:"white"}}>
             <div className="dashboardInfo col-md-4">
               <h4 className="text-center">Total <br/> Projects</h4>
@@ -59,22 +65,25 @@ function Dashboard({userSession}){
               <h4 className="text-center"><i className="fa-solid fa-user-group"></i> Friends</h4>
               <p className="text-center">{friends}</p>
             </div>
+
             <div className="dashboardInfo col-md-4">
-              <h4 className="text-center">Friend Request</h4>
-              <p className="text-center"><button type='button' className="btn btn-warning btn-sm">View</button></p>
+              <h4 className="text-center">Friend Requests</h4>
+              <p className="text-center"><button type='button' className="btn btn-warning btn-sm" data-bs-toggle="offcanvas" data-bs-target="#getFriendRequest" onClick={getFriendRequest}>View Requests</button></p>
             </div>
+
             <div className="dashboardInfo col-md-4">
               <h4 className="text-center">Add Friend</h4>
-              <p className="text-center"><button className="btn btn-warning btn-sm"  data-bs-toggle="offcanvas" data-bs-target="#developersContact">Find a Dev</button></p>
+              <p className="text-center"><button className="btn btn-warning btn-sm"  data-bs-toggle="offcanvas" data-bs-target="#addFriend" onClick={getDevelopers}>Find a Dev</button></p>
             </div>
+
           </div>
         </div>
 
         <div id="OffcanvasSection">
-          <div className="offcanvas offcanvas-end" tabIndex="-1" id="developersContact" aria-labelledby="developersContactList" style={{backgroundColor:"#05204a"}}>
+          <div className="offcanvas offcanvas-end" tabIndex="-1" id="addFriend" aria-labelledby="addFriendList" style={{backgroundColor:"#05204a"}}>
 
             <div className="offcanvas-header">
-              <h5 id="developersContacts" className="text-light">Add a friend <i className="fa-solid fa-bugs"></i></h5>
+              <h5 id="addFriends" className="text-light">Add a friend <i className="fa-solid fa-user-group"></i></h5>
               <button type="button" className="btn-close text-reset" id='closeOffCanvas' data-bs-dismiss="offcanvas" aria-label="Close" style={{backgroundColor:"gold"}}></button>
             </div><hr/>
 
@@ -108,8 +117,64 @@ function Dashboard({userSession}){
                             .then( data => {
                               document.getElementById('closeOffCanvas').click();
                             })
-                          
                           }}>Add</button>
+                        </td>
+                        
+                      </tr>
+                      )}
+                    </tbody>
+                  </table>
+                </>
+              )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="OffcanvasSection">
+          <div className="offcanvas offcanvas-start" tabIndex="-1" id="getFriendRequest" aria-labelledby="getFriendRequestList" style={{backgroundColor:"#05204a"}}>
+
+            <div className="offcanvas-header">
+              <h5 id="friendRequests" className="text-light">Friend Requests<i className="fa-solid fa-user-group"></i></h5>
+              <button type="button" className="btn-close text-reset" id='closeFriendReqOffCanvas' data-bs-dismiss="offcanvas" aria-label="Close" style={{backgroundColor:"gold"}}></button>
+            </div><hr/>
+
+
+            <div className="row offcanvas-body">
+              <div className="col-12">
+                {(typeof friendRequest==="string")?(
+                  <div className="row align-items-center" style={{minHeight:'400px'}}>
+                    <div className="col">
+                      <h4 className="text-center" style={{color:"grey"}}>No Friend Requests Available</h4>
+                    </div>
+                  </div>
+                ):(
+                <>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>S/N</th>
+                        <th>Developer</th>
+                        <th>Stack</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {Object.values(friendRequest).map(dev => 
+                      <tr>
+                        <td key={dev.serial_no}>{dev.serial_no}</td>
+                        <td key={dev.dev_nickname}>{dev.dev_nickname[0].toUpperCase()  + dev.dev_nickname.substring(1)}</td>
+                        <td key={`stack ${dev.dev_stack}`}>{dev.dev_stack}</td>
+                        <td key={`message${dev.dev_id}`}>
+                          <button className="btn btn-warning btn-sm" id={`btnSendRequest${dev.dev_id}`} onClick={()=>{
+
+                            fetch(`http://localhost:5000/api/v1/accept-friend-request/?invitee=${userSession}&invited=${dev.dev_id}`)
+                            .then( rsp => rsp.json())
+                            .then( data => {
+                              alert(data.message);
+                              document.getElementById('closeFriendReqOffCanvas').click();
+                            })
+                          }}>Accept</button>
                         </td>
                         
                       </tr>
